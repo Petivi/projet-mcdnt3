3dc23df2f8b09a6517c9e6cb8958203d3bf3e078
neskwik93 <eric.bauman@laposte.net>
2018-08-23 16:06:40 +0200

    initialisation projet

--- wow-planner/src/app/app.component.ts
+++ wow-planner/src/app/app.component.ts
@@ -12,13 +12,15 @@ export class AppComponent implements OnInit {
 
     constructor(private _appService: AppService, private http: HttpClient) { }
     ngOnInit() {
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
         console.log(this._appService.getTest());
         this.http.get("http://localhost/wow-planner/test.php").subscribe(res => {
             console.log(res)
         });
         let coucou = 'mabite';
-        this.http.post("http://localhost/wow-planner/testEnvoi.php", JSON.stringify({coucou:coucou})).subscribe(res => {
-            console.log(res);
+        this.http.post("http://localhost/wow-planner/testEnvoi.php", JSON.stringify({ coucou: coucou })).subscribe(res => {
+            console.log(res)
         });
     }
 }
