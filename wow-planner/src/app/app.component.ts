17702b693658101bfad547ef2ae84b60afd12a70
Petivi <33059675+Petivi@users.noreply.github.com>
2018-09-17 10:06:24 +0200

    quick test

--- wow-planner/src/app/app.component.ts
+++ wow-planner/src/app/app.component.ts
@@ -13,6 +13,15 @@ export class AppComponent implements OnInit {
 
     constructor(private _appService: AppService, private http: HttpClient, private _router: Router) { }
     ngOnInit() {
+      let user = {
+           lastname: 'Admin', firstname: 'Admin', pseudo: 'admin',
+           password: 'admin', mail: 'admin@admin.admin'
+       };
+       this.http.post("http://localhost/wow-planner-app/action/addNewUser.php", JSON.stringify(user)).catch((err) => {
+           return Observable.throw(err);
+       }).subscribe(res => {
+           console.log(res)
+       });
 
     }
 }
