9e55d4f83f8aef8d335329bd4b92461aed20b486
Petivi <33059675+Petivi@users.noreply.github.com>
2018-09-17 16:15:11 +0200

    addWords + edit comment getWords

---
+++ www/php/action/addWords.php
@@ -0,0 +1,56 @@
+<?php
+
+// need user's pseudo to be check his permissions
+if(isset($request->pseudo)){
+  $pseudo = htmlspecialchars($request->pseudo, ENT_QUOTES);
+
+  // check if user exists
+  $check_user_info = 'SELECT * FROM users WHERE pseudo = $pseudo AND active_account LIKE 1';
+  $check_user_info = $base->prepare($check_user_info);
+  $check_user_info->execute();
+  while($user_info = $check_user_info->fetch())
+  {
+    // get his permissions
+    $user_permissions = $user_info['permissions'];
+  }
+
+  // if admin
+  if($user_permissions == 'masterofpuppets'){
+
+    // get message name and its translations + page to display on
+    if(isset($request->msg_name)){
+      $msg_name = htmlspecialchars($request->msg_name, ENT_QUOTES);
+    }else{
+      $msg_name = "";
+    }
+    if(isset($request->msg_fr)){
+      $msg_fr = htmlspecialchars($request->msg_fr, ENT_QUOTES);
+    }else{
+      $msg_fr = "";
+    }
+    if(isset($request->msg_en)){
+      $msg_en = htmlspecialchars($request->msg_en, ENT_QUOTES);
+    }else{
+      $msg_en = "";
+    }
+    if(isset($request->page)){
+      $page = htmlspecialchars($request->page, ENT_QUOTES);
+    }else{
+      $page = "";
+    }
+
+    // insert the new word in database
+    $add_new_message = 'INSERT INTO messages (msg_name, msg_fr, msg_en, page)
+    VALUES (:msg_name, :msg_fr, :msg_en, :page)';
+    $add_new_message = $base->prepare($add_new_message);
+    $add_new_message->bindValue('msg_name', $msg_name, PDO::PARAM_STR);
+    $add_new_message->bindValue('msg_fr', $msg_fr, PDO::PARAM_STR);
+    $add_new_message->bindValue('msg_en', $msg_en, PDO::PARAM_STR);
+    $add_new_message->bindValue('page', $page, PDO::PARAM_STR);
+    $add_new_message->execute();
+
+  }
+}
+
+
+ ?>
--- www/php/action/getWords.php
+++ www/php/action/getWords.php
@@ -4,18 +4,26 @@ include "../includedFiles.php";
 
 
 $listWords = array();
+// request to get every words in database (with every language)
 $get_words_language = 'SELECT * FROM messages';
 $get_words_language = $base->prepare($get_words_language);
 $get_words_language->execute();
 while($words = $get_words_language->fetch())
 {
-  $listWords[$words['msg_name']] = array(
-    'fr' => $words['msg_fr'],
-    'en' => $words['msg_en'],
-    'page' => $words['page'],
+  $msg_name = $words['msg_name'];
+  $msg_fr = $words['msg_fr'];
+  $msg_en = $words['msg_en'];
+  $page = $words['page'];
+
+// create a tab with the message name as index of the tab, and another tab with every language for the desired word
+  $listWords[$msg_name] = array(
+    'fr' => $msg_fr,
+    'en' => $msg_en,
+    'page' => $page,
   );
 }
 
+// call our return function
 echo returnResponse($listWords);
 
  ?>
