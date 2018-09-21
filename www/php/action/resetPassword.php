eb3c752ee7db06789c4997a7c75121c5e83e7dab
Petivi <33059675+Petivi@users.noreply.github.com>
2018-09-21 16:16:08 +0200

    .

--- www/php/action/resetPassword.php
+++ www/php/action/resetPassword.php
@@ -59,11 +59,12 @@ if(isset($request->mail)){
       }
 
       sendMailResetPass($lastname, $firstname, $pseudo, $mail, $token_temp, $lang);
-      echo returnResponse("Mail Sent");
     }
+    echo returnResponse("Mail Sent");
 
   }else {
     echo returnError("No mail");
+    exit();
   }
 
 
--- www/php/include/envoiMail.php
+++ www/php/include/envoiMail.php
@@ -81,11 +81,11 @@ function sendMailResetPass($lastname, $firstname, $pseudo, $mail, $token_temp, $
   if($lang == "fr"){
     $subject = "Réinitialisation de mot de passe";
     $html = "Bonjour ".$pseudo.", veuillez <a href='".$activateLink."/".$token_temp."'>Cliquez sur ce lien pour réinitialiser votre mot de passe</a>
-    Votre lien est valable pendant ".$nb_jour_token_expiration;
+    <br>Votre lien est valable pendant ".$nb_jour_token_expiration;
   }else {
     $subject = "Password reset";
     $html = "Hello ".$pseudo.", please <a href='".$activateLink."/".$token_temp."'>Click on that link to reset your password</a>
-    Your link is available for ".$nb_jour_token_expiration;
+    <br>Your link is available ".$nb_jour_token_expiration;
   }
   require('../PHPMail/V2.0/Mailin.php');
   $mailin = new Mailin('https://api.sendinblue.com/v2.0', $sendinblue_access_key, 5000);    //Optional parameter: Timeout in MS
