7ece8e78d868d3f5eea98d015e82b9918cdb1e19
Petivi <33059675+Petivi@users.noreply.github.com>
2018-10-17 17:02:17 +0200

    .

--- www/php/action/contactUs.php
+++ www/php/action/contactUs.php
@@ -14,7 +14,7 @@ if($tabUser['contact_mail'] && $tabUser['contact_subject'] && $tabUser['contact_
   $add_request_contact = 'INSERT INTO requests_contact_list (user_mail, request_subject, request_text, request_ref, request_date)
   VALUES (:user_mail, :request_subject, :request_text, :request_ref, :request_date)';
   $add_request_contact = $base->prepare($add_request_contact);
-  $add_request_contact->bindValue('user_mail', $tabUser['user_mail'], PDO::PARAM_STR);
+  $add_request_contact->bindValue('user_mail', $tabUser['contact_mail'], PDO::PARAM_STR);
   $add_request_contact->bindValue('request_subject', $tabUser['request_subject'], PDO::PARAM_STR);
   $add_request_contact->bindValue('request_text', $tabUser['request_text'], PDO::PARAM_STR);
   $add_request_contact->bindValue('request_ref', $request_ref, PDO::PARAM_STR);
