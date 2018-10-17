f36e05e1d571d2ac640d04fb8bc9d3e4f44f7c6d
Petivi <33059675+Petivi@users.noreply.github.com>
2018-10-17 15:21:31 +0200

    .

--- www/php/action/admin/saveUserInfo.php
+++ www/php/action/admin/saveUserInfo.php
@@ -32,7 +32,7 @@ if(accessToAdminPermissions($tabUser['session_token'])){
   }
 
   if($user_exists){
-    if(returnCheckPseudo($tabVictim['pseudo'])){
+    if(returnCheckPseudo($tabVictim['pseudo']),$tabVictim['id']){
       echo returnError($display_error_pseudo_taken);
       exit();
     }else {
@@ -58,6 +58,7 @@ if(accessToAdminPermissions($tabUser['session_token'])){
 
   }else {
     echo returnError($display_error_empty);
+    exit();
   }
 
 
