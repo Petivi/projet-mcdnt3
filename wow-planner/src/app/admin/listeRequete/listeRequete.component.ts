50d7104fefd0e2ee7b4f2517bc4a30ed9d55f9c8
Neskwik93 <eric.bauman@laposte.net>
2018-10-18 14:52:14 +0200

    .

--- wow-planner/src/app/admin/listeRequete/listeRequete.component.ts
+++ wow-planner/src/app/admin/listeRequete/listeRequete.component.ts
@@ -17,6 +17,8 @@ export class ListeRequeteComponent implements OnInit {
     valid: boolean = true;
     ttRequete: Requete[] = [];
     requeteActive: Requete = null;
+    token: string = null;
+
 
     controls = () => ({
     });
@@ -24,6 +26,8 @@ export class ListeRequeteComponent implements OnInit {
     constructor(private _formBuilder: FormBuilder, private _appService: AppService, private _router: Router) { }
 
     ngOnInit() {
+        if (localStorage.getItem('userConnected')) this.token = JSON.parse(localStorage.getItem('userConnected')).session_token;
+        else this.token = null;
         this.buildControl();
         this._appService.post('action/checkIfAdmin.php', JSON.parse(localStorage.getItem('userConnected'))).then(res => {
             if (res.error) {
@@ -31,7 +35,7 @@ export class ListeRequeteComponent implements OnInit {
             } else {
                 this._appService.post('action/admin/getContactMessagesList.php', JSON.parse(localStorage.getItem('userConnected'))).then(res => {
                     console.log(res)
-                    if(res.response && res.response.length > 0) {
+                    if (res.response && res.response.length > 0) {
                         this.ttRequete = res.response;
                         this.ttRequete.forEach(r => {
                             r.libelle_request_closed = r.request_closed === '0' ? 'non' : 'oui';
@@ -40,7 +44,7 @@ export class ListeRequeteComponent implements OnInit {
                 });
             }
         });
-    } 
+    }
 
     buildControl() {
     }
@@ -54,5 +58,15 @@ export class ListeRequeteComponent implements OnInit {
 
     retour() {
         this.requeteActive = null
+    }
+
+    supprimer(requete) {
+        console.log(requete)
+        console.log(this.token)
+        if (this.token) {
+            this._appService.post('action/admin/deleteContactMessage.php', { session_token: this.token, id: requete.id }).then(res => {
+                console.log(res);
+            });
+        }
     }
 }
