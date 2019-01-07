7a07045744304887ac14a1780afd264bb0facb1b
Neskwik93 <eric.bauman@laposte.net>
2019-01-07 14:36:05 +0100

    .

--- wow-planner/src/app/accueil/accueil.component.ts
+++ wow-planner/src/app/accueil/accueil.component.ts
@@ -10,40 +10,14 @@ import { User } from '../model/app.model';
 })
 export class AccueilComponent implements OnInit, OnDestroy {
     userConnected: User;
-    tabClasses: any [];
-    tabRaces: any [];
-    character: any = {name:"", race_id:null, class_id:null};
     constructor(private _appService: AppService) { }
 
     ngOnInit() {
         this._appService.setPage('accueil');
-        this.getRaces();
-        this.getClasses();
     }
 
     ngOnDestroy() {
 
     }
 
-    getRaces(){
-      this._appService.post('action/api-blizzard/api-blizzard.php',
-      {url_missing: 'data/character/races', tabParam: [{key: 'locale', value: 'fr_UE'}] }).then(res => {
-        this.tabRaces = res.races;
-        console.log(this.tabRaces)
-      });
-    }
-    getClasses(){
-      this._appService.post('action/api-blizzard/api-blizzard.php',
-      {url_missing: 'data/character/classes', tabParam: [{key: 'locale', value: 'fr_UE'}] }).then(res => {
-        this.tabClasses = res.classes;
-        console.log(this.tabClasses)
-      });
-    }
-    validChar(){
-      this._appService.post('action/api-blizzard/addNewCharacter.php',
-      {session_token: JSON.parse(localStorage.getItem("userConnected")).session_token, character:this.character}).then(res => {
-
-      });
-      console.log(this.character);
-    }
 }
