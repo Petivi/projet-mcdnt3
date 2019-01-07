6ced01ac49a733f5a1568d27edf1d8b45be70d1a
Petivi <33059675+Petivi@users.noreply.github.com>
2019-01-07 14:54:46 +0100

    Revert "Revert ".""
    This reverts commit 66b6ca2d40c46ace6d527ba795b2c07873d0d67c.

--- wow-planner/src/app/creationPersonnage/creationPersonnage.component.ts
+++ wow-planner/src/app/creationPersonnage/creationPersonnage.component.ts
@@ -1,18 +1,56 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AppService } from '../app.service';
+
+import { Word } from '../model/app.model';
 
 @Component({
     selector: 'creation-personnage-cpt',
     templateUrl: './creationPersonnage.component.html',
 })
-export class CreationPersonnageComponent implements OnInit, OnDestroy {
+export class CreationPersonnageComponent implements OnInit {
+    words: Word[] = [];
+    tabClasses: any[];
+    tabRaces: any[];
+    character: any = { name: "", race_id: null, class_id: null };
 
-    constructor() { }
+    constructor(private _appService: AppService) { }
 
     ngOnInit() {
-
+        this._appService.getWords(['common']).then(res => {
+            res.forEach(w => {
+                this.words.push(w);
+            });
+            console.log(this.words)
+        });
+        this._appService.setPage('accueil');
+        this.getRaces();
+        this.getClasses();
+        this._appService.post('action/api-blizzard/api-blizzard.php',
+            { url_missing: 'challenge/hyjal', tabParam: [{ key: 'locale', value: 'fr_UE' }] }).then(res => {
+                console.log(res)
+            });
     }
 
-    ngOnDestroy() {
+    getRaces() {
+        this._appService.post('action/api-blizzard/api-blizzard.php',
+            { url_missing: 'data/character/races', tabParam: [{ key: 'locale', value: 'fr_UE' }] }).then(res => {
+                this.tabRaces = res.races;
+                console.log(this.tabRaces)
+            });
+    }
+    getClasses() {
+        this._appService.post('action/api-blizzard/api-blizzard.php',
+            { url_missing: 'data/character/classes', tabParam: [{ key: 'locale', value: 'fr_UE' }] }).then(res => {
+                this.tabClasses = res.classes;
+                console.log(this.tabClasses)
+            });
+    }
+    validChar() {
+        this._appService.post('action/api-blizzard/addNewCharacter.php',
+            { session_token: JSON.parse(localStorage.getItem("userConnected")).session_token, character: this.character }).then(res => {
 
+            });
+        console.log(this.character);
     }
 }
