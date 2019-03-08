97651f417f61bf47f0b8fabe46adb4b310f7e3cb
Petivi <33059675+Petivi@users.noreply.github.com>
2019-03-08 14:57:25 +0100

    .

--- www/php/action/api-blizzard/getComments.php
+++ www/php/action/api-blizzard/getComments.php
@@ -4,6 +4,19 @@ include "../../includedFiles.php";
 
 $tabInfo = commentManagement($request);
 
+if($tabInfo['session_token']){
+  $account_id = NULL;
+  $request_user_info = 'SELECT * FROM users WHERE session_token LIKE :session_token';
+  $request_user_info = $base->prepare($request_user_info);
+  $request_user_info->bindValue('session_token', $tabInfo['session_token'], PDO::PARAM_STR);
+  $request_user_info->execute();
+  while($user_info = $request_user_info->fetch())
+  {
+    // get his informations
+    $account_id = $user_info['id'];
+  }
+}
+
 
 if($tabInfo['character_id']){
 
@@ -14,14 +27,19 @@ if($tabInfo['character_id']){
   $request_characters_comment->execute();
   while($comment_info = $request_characters_comment->fetch())
   {
+    if($comment_info['user_id'] == $account_id){
+      $editable = true;
+    }else {
+      $editable = false;
+    }
     array_push($tabCharactersComment, array(
       "comment_id" => $comment_info['id'],
-      "user_id" => $comment_info['user_id'],
       "user_pseudo" => Chiffrement::decrypt($comment_info['user_pseudo']),
       "character_id" => $comment_info['character_id'],
       "comment" => $comment_info['comment'],
       "created_date" => date('d/m/Y H:i:s', $comment_info['created_date']),
       "last_modified" => date('d/m/Y H:i:s', $comment_info['last_modified']),
+      "editable" => $editable
     ));
   }
 
