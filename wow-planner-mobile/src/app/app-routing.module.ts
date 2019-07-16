40a10e24ea5e3ce03cfcea05c74b2b0f7f07f452
Neskwik93 <eric.bauman@laposte.net>
2019-07-16 17:00:26 +0200

    .

--- wow-planner-mobile/src/app/app-routing.module.ts
+++ wow-planner-mobile/src/app/app-routing.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
