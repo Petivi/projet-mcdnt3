da49f68f77fb43a5254aa8ef221c700e7f472096
Petivi <33059675+Petivi@users.noreply.github.com>
2019-08-21 15:31:10 +0200

    Faut faire les fonctions mais la flemme xD

--- wow-planner-mobile/src/app/affichage-personnage/affichage-personnage.page.ts
+++ wow-planner-mobile/src/app/affichage-personnage/affichage-personnage.page.ts
@@ -10,13 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 export class AffichagePersonnagePage implements OnInit {
 
   obsInit: Subscription;
+  ttItems: any[] = [];
+  words: Word[] = [];
 
   constructor(private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.obsInit = this._activatedRoute.data.subscribe(res => {
-      console.log(res)
+      console.log(res.affichagePersonnage.character)
+      this.ttItems = res.affichagePersonnage.character;
+      this.words = res.affichagePersonnage.words;
+      // this.ttItems = res.affichagePersonnage.character && res.accueil.affichagePersonnage.character.length > 0 ? res.affichagePersonnage.character : [];
+      console.log(this.words);
     })
+  }
+
+  getWord(libelle: string) {
+    return this.words.find(w => w.msg_name === libelle).value;
   }
 
 }
