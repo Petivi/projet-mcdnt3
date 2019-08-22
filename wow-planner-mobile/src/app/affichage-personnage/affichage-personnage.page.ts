37644142721c421190b0f1eed1d7d7aa89e4f140
Neskwik93 <eric.bauman@laposte.net>
2019-08-22 12:29:48 +0200

    .

--- wow-planner-mobile/src/app/affichage-personnage/affichage-personnage.page.ts
+++ wow-planner-mobile/src/app/affichage-personnage/affichage-personnage.page.ts
@@ -1,45 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+
 import * as globals from '../../assets/data/globals';
 
+import { AppService } from '../app.service';
+
+import { Word } from '../model/app.model';
 
 @Component({
-  selector: 'app-affichage-personnage',
-  templateUrl: './affichage-personnage.page.html',
-  styleUrls: ['./affichage-personnage.page.scss'],
+    selector: 'app-affichage-personnage',
+    templateUrl: './affichage-personnage.page.html',
+    styleUrls: ['./affichage-personnage.page.scss'],
 })
 export class AffichagePersonnagePage implements OnInit {
 
-  obsInit: Subscription;
-  ttItems: any[] = [];
-  words: Word[] = [];
-  ttBonusStats: any[] = [];
+    obsInit: Subscription;
+    ttItems: any[] = [];
+    words: Word[] = [];
+    ttBonusStats: any[] = [];
 
-  constructor(private _activatedRoute: ActivatedRoute) { }
+    constructor(private _activatedRoute: ActivatedRoute, private _appService: AppService) { }
 
-  ngOnInit() {
-    this.obsInit = this._activatedRoute.data.subscribe(res => {
-      this.ttItems = res.affichagePersonnage.character;
-      this.words = res.affichagePersonnage.words;
-      this.ttBonusStats = globals.bonusStats.map(bs => {
-        if (this._appService.getLangue() === 'fr') {
-          return { id: bs.id, libelle: bs.nameFr }
-        } else {
-          return { id: bs.id, libelle: bs.nameEn }
-        }
-      });
-      // this.ttItems = res.affichagePersonnage.character && res.accueil.affichagePersonnage.character.length > 0 ? res.affichagePersonnage.character : [];
-      console.log(this.ttItems);
-    })
-  }
+    ngOnInit() {
+        this.obsInit = this._activatedRoute.data.subscribe(res => {
+            this.ttItems = res.affichagePersonnage.character;
+            this.words = res.affichagePersonnage.words;
+            this.ttBonusStats = globals.bonusStats.map(bs => {
+                if (this._appService.getLangue() === 'fr') {
+                    return { id: bs.id, libelle: bs.nameFr }
+                } else {
+                    return { id: bs.id, libelle: bs.nameEn }
+                }
+            });
+            // this.ttItems = res.affichagePersonnage.character && res.accueil.affichagePersonnage.character.length > 0 ? res.affichagePersonnage.character : [];
+            console.log(this.ttItems);
+        })
+    }
 
-  getWord(libelle: string) {
-    return this.words.find(w => w.msg_name === libelle).value;
-  }
+    getWord(libelle: string) {
+        return this.words.find(w => w.msg_name === libelle).value;
+    }
 
-  getHealthLibelle() {
-		return this.ttBonusStats.find(bs => bs.id === 1).libelle;
-	}
+    getHealthLibelle() {
+        return this.ttBonusStats.find(bs => bs.id === 1).libelle;
+    }
 
 }
